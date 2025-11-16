e823cf361c610fccfaed76e099a57e4b1ab355ae
saadnadeem27 <saad27apcoms01@gmail.com>
2025-11-17 01:06:58 +0500

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:contento/config/theme/light_theme.dart';
+import 'package:contento/firebase_options.dart';
 import 'package:contento/view/screens/launch/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -12,6 +18,8 @@ String dummyImg =
     'https://images.unsplash.com/photo-1558507652-2d9626c4e67a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80';
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
